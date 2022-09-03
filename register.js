f95aabb9476e2d65055da3868c7d8dd087d561f4
Havochalo <officialmail85@gmail.com>
2022-09-03 14:33:18 +1000

    First commit

---
+++ register.js
@@ -0,0 +1,21 @@
+function validateRegister()
+{
+
+    // var firstName = document.getElementById("firstName").value;
+    // var lastName = document.getElementById("lastName").value;
+    // var email = document.getElementById("emailAddress").value;
+    // var password = document.getElementById("password").value;
+    // var confirmPassword = document.getElementById("confirmPassword").value;
+
+    // if (password == confirmPassword)
+    // {
+    //     alert("Register Sucessfull");
+    // }
+    // else
+    // {
+    //     alert("Not ducess");
+    // }
+
+    alert("Hello");
+
+}
